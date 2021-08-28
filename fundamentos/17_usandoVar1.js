81fcd4976cd1891fab267190f1d46caa6f7b34f4
rodolforomera <rodolforomeras@gmail.com>
2021-08-28 14:04:00 -0300

    Exercicios fundamentos javascript

---
+++ fundamentos/17_usandoVar1.js
@@ -0,0 +1,19 @@
+{
+    {
+        {
+            { 
+                var sera = 'Será???'
+                console.log(sera); // Será???
+            }
+        }
+    }
+}
+console.log(sera); // Será???
+
+function teste() {
+    var local = 123;
+    console.log(local);
+}
+
+teste();
+// console.log(local); // nesse caso var só está disponivel dentro do escopo da função
---
+++ fundamentos/24_hoisting.js
@@ -0,0 +1,16 @@
+console.log('a =', a); //undefined
+var a = 2;
+console.log('a =', a); //2
+
+/**
+ * Javascript Hoisting:
+ * var a;
+ * console.log('a =', a); //undefined
+ * a = 2;
+ * console.log('a =', a); //2
+*/
+
+
+//console.log('b =', b); //Erro
+let b = 2;
+console.log('b =', b); //2
