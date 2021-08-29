5aa5474de7011739435cf578babae9ddb6d4ee3a
rodolforomera <rodolforomeras@gmail.com>
2021-08-29 10:49:58 -0300

    Exercicios fundamentos javascript

---
+++ fundamentos/27_notacaoPonto.js
@@ -0,0 +1,19 @@
+console.log(Math.ceil(6.1)); // 7
+
+const obj1 = {};
+obj1.nome = 'Bola';
+//obj1['nome'] = 'Bola2';
+console.log(obj1.nome); // Bola
+
+function Obj(nome) {
+    this.nome = nome;
+    this.exec = function () {
+        console.log('Exec...');
+    }
+}
+
+const obj2 = new Obj('Cadeira');
+const obj3 = new Obj('Mesa');
+console.log(obj2.nome); // Cadeira
+console.log(obj3.nome); // Mesa
+obj3.exec() // Exec...
