5f145b35fcba839c378829ee583f1f577c6760e4
rodolforomera <rodolforomeras@gmail.com>
2021-09-01 15:15:48 -0300

    Exercicios objeto javascript

---
+++ objeto/08_heranca1.js
@@ -0,0 +1,20 @@
+const ferrari = {
+    modelo: 'F40',
+    velMax: 324
+}
+
+const volvo = {
+    modelo: 'V40',
+    velMax: 200
+}
+
+console.log(ferrari.prototype); // undefined
+console.log(ferrari.__proto__); // [Object: null prototype] {} 
+console.log(ferrari.__proto__ === Object.prototype); // true
+console.log(volvo.__proto__ === Object.prototype); // true
+console.log(Object.prototype.__proto__); // null
+console.log(Object.prototype.__proto__ === null); // true
+
+function MeuObjeto() {}
+console.log(typeof Object, typeof MeuObjeto); // function function
+console.log(Object.prototype, MeuObjeto.prototype); // [Object: null prototype] {} {} 
