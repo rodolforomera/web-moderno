b8705aced45b2f4c4a53b0f3e30ae0e9ebfa2014
rodolforomera <rodolforomeras@gmail.com>
2022-01-06 22:17:02 -0300

    Exercicios express

---
+++ exercicios-express/api/usuario.js
@@ -0,0 +1,10 @@
+function salvar(req, res){
+    res.send('Usuario > salvar')
+}
+
+function obter(req, res){
+    res.send('Usuario > obter')
+}
+
+//module.exports = { salvar: salvar, obter: obter }
+module.exports = { salvar, obter }
